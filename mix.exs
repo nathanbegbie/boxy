47370bedc68b9a9420f5cdc2a238e54311b9a023
nathanbegbie <nathanbegbie@gmail.com>
2022-07-17 07:08:59 +0300

    add dev tools credo and dialyzer

--- mix.exs
+++ mix.exs
@@ -45,7 +45,9 @@ defmodule Boxy.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5.0", only: [:dev], runtime: false}
     ]
   end
 
